50daef51393701d5b32f46828555efe46c52f26c
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2015-10-23 21:01:38 +0330

    issing files...

--- Solution Items/Install SignalR Service.bat
+++ Solution Items/Install SignalR Service.bat
@@ -1,6 +1,6 @@
 cd /d %~dp0
 
-"..\Bin\SignalR Library\SignalRServer\SignalRServer.exe" /i
+"..\Out\SignalR Library\SignalRServer\SignalRServer.exe" /i
 
 netsh http add urlacl http://127.0.0.1:50023/ user=EveryOne    
 
